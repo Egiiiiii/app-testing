4c470f013cb6346c29ff025ac14e18d2dcd10c8d
eggoreng <151418949+Egiiiiii@users.noreply.github.com>
2025-12-10 16:03:52 +0700

    error

--- simple-app/index.js
+++ simple-app/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // route GET
 app.get('/', (req, res) => {
-    res.send('Hello World! Node.js server is runningsss.');
+    res.send('Hello World! Node.js server is runningsss.')
 });
 
 // route POST contoh
